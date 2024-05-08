6e059d0b0bbc29e5cb8ce4f35750361a98d9dd5b
Primo18 <danilobo.elo@gmail.com>
2024-05-07 22:36:35 -0400

    chore: Update ButtonRutina component propTypes

--- src/components/ButtonRutina.jsx
+++ src/components/ButtonRutina.jsx
@@ -1,8 +1,17 @@
-import { RiArrowRightLine, RiSearch2Line } from '@remixicon/react';
+// import { RiArrowRightLine, RiSearch2Line } from '@remixicon/react';
 import { Button } from '@tremor/react';
+import { props } from 'prop-types';
 
-export const ButtonHero = ({onClick, buttonName, color}) => (
+
+export const ButtonHero = ({ onClick, buttonName, color }) => (
   <div className='mb-2'>
-      <Button variant="primary" color={color} size="sm" onClick={onClick}>{buttonName}</Button>
+    <Button variant="primary" color={color} size="sm" onClick={onClick}>{buttonName}</Button>
   </div>
 );
+
+
+ButtonHero.propTypes = {
+  onClick: props.func,
+  buttonName: props.string,
+  color: props.string
+}
